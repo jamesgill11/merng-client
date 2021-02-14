a2d7e07e6e086c4798248e8b814755c0e6421443
james gill <jimmygill28@hotmail.com>
2021-02-14 11:15:53 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./utils/authRoute";
+
+import MenuBar from "./Components/MenuBar";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import SinglePost from "./Pages/SinglePost";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="ui container">
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
